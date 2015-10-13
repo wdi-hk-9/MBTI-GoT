c400984fd32ca81031665f5521545f09000211fc
Jacky Lee <jackywhlee@gmail.com>
2015-10-13 14:59:29 +0800

    added progress bar and a total of 10 questions

--- js/questions.js
+++ js/questions.js
@@ -55,5 +55,189 @@ var fullQuestions = {
       type: "thinking",
       point: 2
     }
-  }
+  },
+  q3: {
+    question: "You enjoy having a wide circle of acquaintances",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q4: {
+    question: "You feel involved when watching TV soaps",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q5: {
+    question: "You are usually the first to react to a sudden event",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q6: {
+    question: "You feel that the world is founded on compassion",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q7: {
+    question: "You think that everything in the world is relative",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q8: {
+    question: "Strict observance of the established rules is likely to prevent attaining a good outcome",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q9: {
+    question: "It is difficult to get you excited",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q10: {
+    question: "When making a decision, you rely more on your feelings than on analysis of the situation",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
 }
--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 $(function(){
 
+  var currentQuestion = 1;
+
   var goQuiz = function(){
     $('#start-quiz').click(function(){
     window.location = "./quiz.html";
     })
   }
 
-  var goQuestions = fullQuestions["q1"]["question"]
+  var questionsPool = fullQuestions["q"+ currentQuestion]["question"];
 
   var loadQuestions = function(){
-    $('#questions-full').html(goQuestions)
+    $('#questions-full').html(questionsPool);
   }
+
+  var progress = function(){
+    var currentQPercentage = (currentQuestion - 1) / 50 * 100
+    $('.progress-bar').attr("style", "width:" + currentQPercentage + "%");
+    console.log("changing progress bar");
+  };
+
+  var nextQuestion = $('.btn').click(function(){
+    currentQuestion += 1;
+    var nextNum = currentQuestion;
+    var nextQ = fullQuestions["q"+nextNum]["question"];
+    $('#questions-full').html(nextQ);
+    progress();
+  })
+
 
   goQuiz();
   loadQuestions();

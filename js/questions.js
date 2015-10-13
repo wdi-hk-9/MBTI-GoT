50a2ae7ef1324860f9ab6ec9879978d6f91b4e0a
Jacky Lee <jackywhlee@gmail.com>
2015-10-13 10:05:00 +0800

    added background image, quiz loads question1 from hash, and am not 100% on indentations but should be accurate to my knowledge

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+var totalScore = {
+  extraversion: 0,
+  introversion: 0,
+  sensing: 0,
+  intuition: 0,
+  thinking: 0,
+  feeling: 0,
+  judging: 0,
+  perceiving: 0,
+}
+
+var fullQuestions = {
+  q1: {
+    question: "You are almost never late for your appointments",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  },
+  q2: {
+    question: "You like to be engaged in an active and fast-paced job.",
+    option1: {
+      type: "feeling",
+      point: 2
+    },
+    option2: {
+      type: "feeling",
+      point: 1
+    },
+    option3: {
+      type: "none",
+      point: 0
+    },
+    option4: {
+      type: "thinking",
+      point: 1
+    },
+    option5: {
+      type: "thinking",
+      point: 2
+    }
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 $(function(){
 
-  var goQuiz = function() {
+  var goQuiz = function(){
     $('#start-quiz').click(function(){
     window.location = "./quiz.html";
     })
   }
 
+  var goQuestions = fullQuestions["q1"]["question"]
+
+  var loadQuestions = function(){
+    $('#questions-full').html(goQuestions)
+  }
+
   goQuiz();
+  loadQuestions();
 
 })

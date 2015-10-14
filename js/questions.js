4b63b3390d1eace9c8074af9d9df0a06df6a9f31
Jacky Lee <jackywhlee@gmail.com>
2015-10-14 22:53:38 +0800

    css overhaul on button and quiz-box

--- js/questions.js
+++ js/questions.js
@@ -16,14 +16,6 @@ var Quiz = function() {
   this.questions = [
     {
       question: "You are almost never late for your appointments.",
-      option1: { type: "feeling",  point: 2 },
-      option2: { type: "feeling",  point: 1 },
-      option3: { type: "none",     point: 0 },
-      option4: { type: "thinking", point: 1 },
-      option5: { type: "thinking", point: 2 }
-    },
-    {
-      question: "You like to be engaged in an active and fast-paced job.",
       option1: { type: "judging",  point: 2 },
       option2: { type: "judging",  point: 1 },
       option3: { type: "none",     point: 0 },
@@ -31,47 +23,39 @@ var Quiz = function() {
       option5: { type: "perceiving", point: 2 }
     },
     {
-      question: "You enjoy having a wide circle of acquaintances.",
-      option1: { type: "sensing",  point: 2 },
-      option2: { type: "sensing",  point: 1 },
+      question: "You like to be engaged in an active and fast-paced job.",
+      option1: { type: "extraversion",  point: 2 },
+      option2: { type: "extraversion",  point: 1 },
       option3: { type: "none",     point: 0 },
-      option4: { type: "intuition", point: 1 },
-      option5: { type: "intuition", point: 2 }
+      option4: { type: "introversion", point: 1 },
+      option5: { type: "introversion", point: 2 }
     },
     {
-      question: "You feel involved when watching TV soaps",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You enjoy having a wide circle of acquaintances.",
+      option1: { type: "extraversion",  point: 2 },
+      option2: { type: "extraversion",  point: 1 },
+      option3: { type: "none",     point: 0 },
+      option4: { type: "introversion", point: 1 },
+      option5: { type: "introversion", point: 2 }
+    },
+    {
+      question: "You feel involved when watching TV soaps.",
+      option1: { type: "introversion",       point: 2 },
+      option2: { type: "introversion",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "extraversion",      point: 1 },
+      option5: { type: "extraversion",      point: 2 }
     },
     {
-      question: "You are usually the first to react to a sudden event",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You are usually the first to react to a sudden event.",
+      option1: { type: "introversion",       point: 2 },
+      option2: { type: "introversion",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "extraversion",      point: 1 },
+      option5: { type: "extraversion",      point: 2 }
     },
     {
-      question: "You feel that the world is founded on compassion",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-    {
-      question: "You think that everything in the world is relative",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-    {
-      question: "Strict observance of the established rules prevents achieving a good outcome",
+      question: "You feel that the world is founded on compassion.",
       option1: { type: "feeling",       point: 2 },
       option2: { type: "feeling",       point: 1 },
       option3: { type: "none",          point: 0 },
@@ -79,7 +63,31 @@ var Quiz = function() {
       option5: { type: "thinking",      point: 2 }
     },
     {
-      question: "It is difficult to get you excited",
+      question: "You think that everything in the world is relative.",
+      option1: { type: "sensing",       point: 2 },
+      option2: { type: "sensing",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "intuition",      point: 1 },
+      option5: { type: "intuition",      point: 2 }
+    },
+    {
+      question: "Strict observance of established rules prevents achieving a good outcome.",
+      option1: { type: "intuition",       point: 2 },
+      option2: { type: "intuition",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "sensing",      point: 1 },
+      option5: { type: "sensing",      point: 2 }
+    },
+    {
+      question: "It is difficult to get you excited.¯\\_(ツ)_/¯",
+      option1: { type: "thinking",       point: 2 },
+      option2: { type: "thinking",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "feeling",      point: 1 },
+      option5: { type: "feeling",      point: 2 }
+    },
+     {
+      question: "When making a decision, you rely more on your feelings than on analysis.",
       option1: { type: "feeling",       point: 2 },
       option2: { type: "feeling",       point: 1 },
       option3: { type: "none",          point: 0 },
@@ -87,63 +95,71 @@ var Quiz = function() {
       option5: { type: "thinking",      point: 2 }
     },
      {
-      question: "When making a decision, you rely more on your feelings than on analysis of the situation",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You often think about humankind and its destiny.",
+      option1: { type: "intuition",       point: 2 },
+      option2: { type: "intuition",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "sensing",      point: 1 },
+      option5: { type: "sensing",      point: 2 }
     },
      {
-      question: "You often think about humankind and its destiny",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You believe the best decision is one which can be easily changed.",
+      option1: { type: "perceiving",       point: 2 },
+      option2: { type: "perceiving",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "judging",      point: 1 },
+      option5: { type: "judging",      point: 2 }
     },
      {
-      question: "You believe the best decision is one which can be easily changed",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You often ponder the root cause of problems.",
+      option1: { type: "intuition",       point: 2 },
+      option2: { type: "intuition",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "sensing",      point: 1 },
+      option5: { type: "sensing",      point: 2 }
     },
      {
-      question: "You often ponder the root cause of phenomena and things",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You prefer to act immediately rather than speculate about various options.",
+      option1: { type: "extraversion",       point: 2 },
+      option2: { type: "extraversion",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "introversion",      point: 1 },
+      option5: { type: "introversion",      point: 2 }
     },
      {
-      question: "You prefer to act immediately rather than speculate about various options",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You trust reason rather than feelings.",
+      option1: { type: "thinking",       point: 2 },
+      option2: { type: "thinking",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "feeling",      point: 1 },
+      option5: { type: "feeling",      point: 2 }
     },
      {
-      question: "You trust reason rather than feelings",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You are inclined to rely more on improvisation than on prior planning.",
+      option1: { type: "perceiving",       point: 2 },
+      option2: { type: "perceiving",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You are inclined to rely more on improvisation than on prior planning",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "judging",      point: 1 },
+      option5: { type: "judging",      point: 2 }
     },
      {
       question: "You spend your leisure time actively socializing with a group of people.",
+      option1: { type: "extraversion",       point: 2 },
+      option2: { type: "extraversion",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "introversion",      point: 1 },
+      option5: { type: "introversion",      point: 2 }
+    },
+     {
+      question: "You usually plan your actions in advance.",
+      option1: { type: "judging",       point: 2 },
+      option2: { type: "judging",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "perceiving",      point: 1 },
+      option5: { type: "perceiving",      point: 2 }
+    },
+     {
+      question: "Your actions are frequently influenced by your emotions.",
       option1: { type: "feeling",       point: 2 },
       option2: { type: "feeling",       point: 1 },
       option3: { type: "none",          point: 0 },
@@ -151,7 +167,79 @@ var Quiz = function() {
       option5: { type: "thinking",      point: 2 }
     },
      {
-      question: "You usually plan your actions in advance",
+      question: "You are a person somewhat reserved and distant in communication.",
+      option1: { type: "introversion",       point: 2 },
+      option2: { type: "introversion",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "extraversion",      point: 1 },
+      option5: { type: "extraversion",      point: 2 }
+    },
+     {
+      question: "You know how to put every minute of your time to good purpose.",
+      option1: { type: "judging",       point: 2 },
+      option2: { type: "judging",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "perceiving",      point: 1 },
+      option5: { type: "perceiving",      point: 2 }
+    },
+     {
+      question: "You often contemplate the complexity of life.",
+      option1: { type: "intuition",       point: 2 },
+      option2: { type: "intuition",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "sensing",      point: 1 },
+      option5: { type: "sensing",      point: 2 }
+    },
+     {
+      question: "After prolonged socializing you feel you need to get away and be alone.",
+      option1: { type: "introversion",       point: 2 },
+      option2: { type: "introversion",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "extraversion",      point: 1 },
+      option5: { type: "extraversion",      point: 2 }
+    },
+     {
+      question: "You often do jobs in a hurry. ٩ʕ•͡×•ʔ۶",
+      option1: { type: "judging",       point: 2 },
+      option2: { type: "judging",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "perceiving",      point: 1 },
+      option5: { type: "perceiving",      point: 2 }
+    },
+     {
+      question: "You easily see the general principle behind specific occurrences.",
+      option1: { type: "intuition",       point: 2 },
+      option2: { type: "intuition",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "sensing",      point: 1 },
+      option5: { type: "sensing",      point: 2 }
+    },
+     {
+      question: "You frequently and easily express your feelings and emotions.",
+      option1: { type: "extraversion",       point: 2 },
+      option2: { type: "extraversion",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "introversion",      point: 1 },
+      option5: { type: "introversion",      point: 2 }
+    },
+     {
+      question: "You find it difficult to speak loudly.",
+      option1: { type: "introversion",       point: 2 },
+      option2: { type: "introversion",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "extraversion",      point: 1 },
+      option5: { type: "extraversion",      point: 2 }
+    },
+     {
+      question: "You get bored if you have to read theoretical books.",
+      option1: { type: "intuition",       point: 2 },
+      option2: { type: "intuition",       point: 1 },
+      option3: { type: "none",          point: 0 },
+      option4: { type: "sensing",      point: 1 },
+      option5: { type: "sensing",      point: 2 }
+    },
+     {
+      question: "You tend to sympathize with other people.",
       option1: { type: "feeling",       point: 2 },
       option2: { type: "feeling",       point: 1 },
       option3: { type: "none",          point: 0 },
@@ -159,100 +247,20 @@ var Quiz = function() {
       option5: { type: "thinking",      point: 2 }
     },
      {
-      question: "Your actions are frequently influenced by your emotions",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      question: "You value justice higher than mercy.",
+      option1: { type: "thinking",       point: 2 },
+      option2: { type: "thinking",       point: 1 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "feeling",      point: 1 },
+      option5: { type: "feeling",      point: 2 }
     },
-     {
-      question: "You are a person somewhat reserved and distant in communication",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
+      {
+      question: "dummy",
+      option1: { type: "thinking",       point: 0 },
+      option2: { type: "thinking",       point: 0 },
       option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You know how to put every minute of your time to good purpose",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You often contemplate the complexity of life",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "After prolonged socializing you feel you need to get away and be alone",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You often do jobs in a hurry",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You easily see the general principle behind specific occurrences",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You frequently and easily express your feelings and emotions",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You find it difficult to speak loudly",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You get bored if you have to read theoretical books",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You tend to sympathize with other people",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
-    },
-     {
-      question: "You value justice higher than mercy",
-      option1: { type: "feeling",       point: 2 },
-      option2: { type: "feeling",       point: 1 },
-      option3: { type: "none",          point: 0 },
-      option4: { type: "thinking",      point: 1 },
-      option5: { type: "thinking",      point: 2 }
+      option4: { type: "feeling",      point: 0 },
+      option5: { type: "feeling",      point: 0 }
     }
     ];
 };
@@ -264,7 +272,7 @@ Quiz.prototype.questionPercentage = function(){
 Quiz.prototype.nextQuestion = function(){
   return this.questions[this.currentQuestion++];
 };
-
+//what's prototype
 Quiz.prototype.answer = function (){
 
 };
--- js/script.js
+++ js/script.js
@@ -6,34 +6,53 @@ $(function(){
     $('.progress-bar').attr("style", "width:" + quiz.questionPercentage() + "%");
   };
 
-  var nextQuestion = function() {
+  var nextQuestion = function(){
     var nextQ = quiz.nextQuestion();
     $('#questions-full').html(nextQ.question);
-    $('.questionOutOf').html('Progress ' + quiz.currentQuestion + '/30')
+    $('.questionOutOf').html('Progress ' + quiz.currentQuestion + '/30');
+    // if ($('.questionOutOf').html("Progress 5/30")) {
+    //   $('.quiz-1').hide();
+    // }
+    // else {
+    //   $('.quiz-1').show();
+    // }
     updateProgress();
   };
 
+  var reset = function(){
+    location.reload();
+  }
+
   var answer = function(event){
-    // calculate points function in quiz prototype
-    // quiz.answer(event. )
     var chosenOption = $(event.target).attr("option-name");
-
     var questionHash = quiz.questions[quiz.currentQuestion - 1];
-
     var questionType = questionHash[chosenOption]["type"];
-
     var questionPoint = questionHash[chosenOption]["point"];
-
     quiz.totalScore[questionType] += questionPoint;
-
     console.log("the points for " + questionType + " is " + quiz.totalScore[questionType]);
-
-
     nextQuestion();
+    if (quiz.currentQuestion === 31) {
+      console.log("working")
+      console.log(quiz.currentQuestion)
+      $('#quiz-1').hide();
+      $('#result').show();
+
+      var letters = [ "I", "E", "J"]
+
+      var html = ""
+
+      html += "<h2>" + letters[0] + letters[1] + letters[2] + "</h2>"
+      console.log(html)
+      $("#result").append(html)
+    };
   };
 
+
+
+
+  $('#btn-reset').click(reset);
   $('.btn-quiz').click(answer);
   nextQuestion();
+  // stopQuiz();
 
 });
-
